6251b93f7d461e9a04e021ec167e6a1fe824d98e
PriyalParikh <128578887+PriyalParikh@users.noreply.github.com>
2023-03-23 14:56:57 +0530

    Delete p3.py

--- p3.py
+++
@@ -1,6 +0,0 @@
-d={'Nepal':'Kathmandu','Italy':'Rome','England':'London' }
-a=str(input("Enter what you want: "))
-items = d.items()  
-for key, value in items:  
-    if value == a: 
-        print(key)
